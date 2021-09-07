5850027a6d7cd9efacb15e91a75cb1310aad8da5
Yiqun Zhang <u7102332@anu.edu.au>
2021-09-07 19:39:03 +0800

    baseline

--- model/msg3d.py
+++ model/msg3d.py
@@ -1,4 +1,6 @@
+import copy
 import sys
+
 sys.path.insert(0, '')
 
 import math
@@ -25,8 +27,9 @@ class MS_G3D(nn.Module):
                  window_stride,
                  window_dilation,
                  embed_factor=1,
-                 activation='relu'):
+                 nonlinear='relu'):
         super().__init__()
+
         self.window_size = window_size
         self.out_channels = out_channels
         self.embed_channels_in = self.embed_channels_out = out_channels // embed_factor
@@ -47,7 +50,8 @@ class MS_G3D(nn.Module):
                 A_binary=A_binary,
                 num_scales=num_scales,
                 window_size=window_size,
-                use_Ares=True
+                use_Ares=True,
+                activation=nonlinear
             )
         )
 
@@ -75,10 +79,9 @@ class MultiWindow_MS_G3D(nn.Module):
                  out_channels,
                  A_binary,
                  num_scales,
-                 window_sizes=[3,5],
+                 window_sizes=[3, 5],
                  window_stride=1,
-                 window_dilations=[1,1]):
-
+                 window_dilations=[1, 1]):
         super().__init__()
         self.gcn3d = nn.ModuleList([
             MS_G3D(
@@ -110,8 +113,16 @@ class Model(nn.Module):
                  num_gcn_scales,
                  num_g3d_scales,
                  graph,
-                 in_channels=3):
+                 in_channels=3,
+                 to_use_final_fc=True,
+                 to_fc_last=True,
+                 frame_len=300,
+                 nonlinear='relu',
+                 **kwargs):
         super(Model, self).__init__()
+
+        # Activation function
+        self.nonlinear_f = activation_factory(nonlinear)
 
         Graph = import_class(graph)
         A_binary = Graph().A_binary
@@ -119,62 +130,110 @@ class Model(nn.Module):
         self.data_bn = nn.BatchNorm1d(num_person * in_channels * num_point)
 
         # channels
+        # c1 = 96
+        # c2 = c1 * 2  # 192
+        # c3 = c2 * 2  # 384
+
         c1 = 96
-        c2 = c1 * 2     # 192
-        c3 = c2 * 2     # 384
+        c2 = c1 * 2  # 192  # Original implementation
+        c3 = c2 * 2  # 384  # Original implementation
 
         # r=3 STGC blocks
-        # self.gcn3d1 = MultiWindow_MS_G3D(3, c1, A_binary, num_g3d_scales, window_stride=1)
-        self.sgcn1 = nn.Sequential(
-            MS_GCN(num_gcn_scales, 3, c1, A_binary, disentangled_agg=True),
-            MS_TCN(c1, c1),
-            MS_TCN(c1, c1))
-        self.sgcn1[-1].act = nn.Identity()
-        self.tcn1 = MS_TCN(c1, c1)
 
-        # self.gcn3d2 = MultiWindow_MS_G3D(c1, c2, A_binary, num_g3d_scales, window_stride=2)
-        self.sgcn2 = nn.Sequential(
-            MS_GCN(num_gcn_scales, c1, c1, A_binary, disentangled_agg=True),
-            MS_TCN(c1, c2, stride=2),
-            MS_TCN(c2, c2))
-        self.sgcn2[-1].act = nn.Identity()
-        self.tcn2 = MS_TCN(c2, c2)
+        self.sgcn1_msgcn = MS_GCN(num_gcn_scales, in_channels, c1, A_binary, disentangled_agg=True,
+                                  **kwargs,
+                                  activation=nonlinear)
+        self.sgcn1_ms_tcn_1 = MS_TCN(c1, c1, activation=nonlinear)
+        self.sgcn1_ms_tcn_2 = MS_TCN(c1, c1, activation=nonlinear)
+        self.sgcn1_ms_tcn_2.act = nn.Identity()
 
-        # self.gcn3d3 = MultiWindow_MS_G3D(c2, c3, A_binary, num_g3d_scales, window_stride=2)
-        self.sgcn3 = nn.Sequential(
-            MS_GCN(num_gcn_scales, c2, c2, A_binary, disentangled_agg=True),
-            MS_TCN(c2, c3, stride=2),
-            MS_TCN(c3, c3))
-        self.sgcn3[-1].act = nn.Identity()
-        self.tcn3 = MS_TCN(c3, c3)
+        self.tcn1 = MS_TCN(c1, c1, **kwargs, activation=nonlinear)
 
-        self.fc = nn.Linear(c3, num_class)
+
+        self.sgcn2_msgcn = MS_GCN(num_gcn_scales, c1, c1, A_binary, disentangled_agg=True,
+                                  **kwargs, activation=nonlinear)
+        self.sgcn2_ms_tcn_1 = MS_TCN(c1, c2, stride=2, activation=nonlinear)
+        # self.sgcn2_ms_tcn_1 = MS_TCN(c1, c2, activation=nonlinear)
+        self.sgcn2_ms_tcn_2 = MS_TCN(c2, c2, activation=nonlinear)
+        self.sgcn2_ms_tcn_2.act = nn.Identity()
+
+        self.tcn2 = MS_TCN(c2, c2, **kwargs, activation=nonlinear)
+
+        # MSG3D
+        self.sgcn3_msgcn = MS_GCN(num_gcn_scales, c2, c2, A_binary, disentangled_agg=True,
+                                  **kwargs,
+                                  activation=nonlinear)
+        self.sgcn3_ms_tcn_1 = MS_TCN(c2, c3, stride=2, activation=nonlinear)
+        # self.sgcn3_ms_tcn_1 = MS_TCN(c2, c3, activation=nonlinear)
+        self.sgcn3_ms_tcn_2 = MS_TCN(c3, c3, activation=nonlinear)
+        self.sgcn3_ms_tcn_2.act = nn.Identity()
+
+        self.tcn3 = MS_TCN(c3, c3, **kwargs, activation=nonlinear)
+
+
+        # 最后一层加一个fc
+        self.to_use_final_fc = to_use_final_fc
+        if self.to_use_final_fc:
+            self.fc = nn.Linear(c3, num_class)
+
+        # Concat multi-skip
+        self.fc_multi_skip = nn.Sequential(
+            # nn.Linear(c1 + c2 + c3, c3),
+            nn.Linear(c1 + c3, c3),
+            self.nonlinear_f,
+            nn.Linear(c3, c3),
+            self.nonlinear_f
+        )
+
+        # For two stream networks
+        self.to_fc_last = to_fc_last
+
+        # Angle with the body center
+        self.to_use_angle_adj = False
+
+        # Attention graph
+        self.to_use_att_conv_layer = False
 
     def forward(self, x):
-
         N, C, T, V, M = x.size()
+
+
         x = x.permute(0, 4, 3, 1, 2).contiguous().view(N, M * V * C, T)
         x = self.data_bn(x)
-        x = x.view(N * M, V, C, T).permute(0,2,3,1).contiguous()
+        x = x.view(N * M, V, C, T).permute(0, 2, 3, 1).contiguous()
 
         # Apply activation to the sum of the pathways
-        # x = F.relu(self.sgcn1(x) + self.gcn3d1(x), inplace=True)
-        x = F.relu(self.sgcn1(x), inplace=True)
+
+        ###### First Component ######
+        x = self.sgcn1_msgcn(x)
+        x = self.sgcn1_ms_tcn_1(x)
+        x = self.sgcn1_ms_tcn_2(x)
+        x = self.nonlinear_f(x)
         x = self.tcn1(x)
+        ###### End First Component ######
 
-        # x = F.relu(self.sgcn2(x) + self.gcn3d2(x), inplace=True)
-        x = F.relu(self.sgcn2(x), inplace=True)
+        ###### Second Component ######
+        x = self.sgcn2_msgcn(x)
+        x = self.sgcn2_ms_tcn_1(x)
+        x = self.sgcn2_ms_tcn_2(x)
+        x = self.nonlinear_f(x)
         x = self.tcn2(x)
+        ###### End Second Component ######
 
-        # x = F.relu(self.sgcn3(x) + self.gcn3d3(x), inplace=True)
-        x = F.relu(self.sgcn3(x), inplace=True)
+        ###### Third Component ######
+        x = self.sgcn3_msgcn(x)
+        x = self.sgcn3_ms_tcn_1(x)
+        x = self.sgcn3_ms_tcn_2(x)
+        x = self.nonlinear_f(x)
         x = self.tcn3(x)
+        ###### End Third Component ######
 
         out = x
         out_channels = out.size(1)
+
         out = out.view(N, M, out_channels, -1)
-        out = out.mean(3)   # Global Average Pooling (Spatial+Temporal)
-        out = out.mean(1)   # Average pool number of bodies in the sequence
+        out = out.mean(3)  # Global Average Pooling (Spatial+Temporal)
+        out = out.mean(1)  # Average pool number of bodies in the sequence
 
         out = self.fc(out)
         return out
@@ -183,6 +242,7 @@ class Model(nn.Module):
 if __name__ == "__main__":
     # For debugging purposes
     import sys
+
     sys.path.append('..')
 
     model = Model(
@@ -195,7 +255,7 @@ if __name__ == "__main__":
     )
 
     N, C, T, V, M = 6, 3, 50, 25, 2
-    x = torch.randn(N,C,T,V,M)
+    x = torch.randn(N, C, T, V, M)
     model.forward(x)
 
     print('Model total # params:', count_params(model))
