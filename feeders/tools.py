85c0acdca554acabe11ea280c1747361e78791c1
Yiqun Zhang <u7102332@anu.edu.au>
2021-10-26 11:24:30 +0800

    null

--- feeders/tools.py
+++ feeders/tools.py
@@ -1,7 +1,38 @@
 import random
+import torch
+import torch.nn.functional as F
 
 import numpy as np
 
+def valid_crop_resize(data_numpy,valid_frame_num,p_interval,window):
+    # input: C,T,V,M
+    C, T, V, M = data_numpy.shape
+    begin = 0
+    end = valid_frame_num
+    valid_size = end - begin
+
+    #crop
+    if len(p_interval) == 1:
+        p = p_interval[0]
+        bias = int((1-p) * valid_size/2)
+        data = data_numpy[:, begin+bias:end-bias, :, :]# center_crop
+        cropped_length = data.shape[1]
+    else:
+        p = np.random.rand(1)*(p_interval[1]-p_interval[0])+p_interval[0]
+        cropped_length = np.minimum(np.maximum(int(np.floor(valid_size*p)),64), valid_size)# constraint cropped_length lower bound as 64
+        bias = np.random.randint(0,valid_size-cropped_length+1)
+        data = data_numpy[:, begin+bias:begin+bias+cropped_length, :, :]
+        if data.shape[1] == 0:
+            print(cropped_length, bias, valid_size)
+
+    # resize
+    data = torch.tensor(data,dtype=torch.float)
+    data = data.permute(0, 2, 3, 1).contiguous().view(C * V * M, cropped_length)
+    data = data[None, None, :, :]
+    data = F.interpolate(data, size=(C * V * M, window), mode='bilinear',align_corners=False).squeeze() # could perform both up sample and down sample
+    data = data.contiguous().view(C, V, M, window).permute(0, 3, 1, 2).contiguous().numpy()
+
+    return data
 
 def downsample(data_numpy, step, random_sample=True):
     # input: C,T,V,M
@@ -157,3 +188,46 @@ def openpose_match(data_numpy):
     data_numpy = data_numpy[:, :, :, rank]
 
     return data_numpy
+
+
+def _rot(rot):
+    """
+    rot: T,3
+    """
+    cos_r, sin_r = rot.cos(), rot.sin()  # T,3
+    zeros = torch.zeros(rot.shape[0], 1)  # T,1
+    ones = torch.ones(rot.shape[0], 1)  # T,1
+
+    r1 = torch.stack((ones, zeros, zeros),dim=-1)  # T,1,3
+    rx2 = torch.stack((zeros, cos_r[:,0:1], sin_r[:,0:1]), dim = -1)  # T,1,3
+    rx3 = torch.stack((zeros, -sin_r[:,0:1], cos_r[:,0:1]), dim = -1)  # T,1,3
+    rx = torch.cat((r1, rx2, rx3), dim = 1)  # T,3,3
+
+    ry1 = torch.stack((cos_r[:,1:2], zeros, -sin_r[:,1:2]), dim =-1)
+    r2 = torch.stack((zeros, ones, zeros),dim=-1)
+    ry3 = torch.stack((sin_r[:,1:2], zeros, cos_r[:,1:2]), dim =-1)
+    ry = torch.cat((ry1, r2, ry3), dim = 1)
+
+    rz1 = torch.stack((cos_r[:,2:3], sin_r[:,2:3], zeros), dim =-1)
+    r3 = torch.stack((zeros, zeros, ones),dim=-1)
+    rz2 = torch.stack((-sin_r[:,2:3], cos_r[:,2:3],zeros), dim =-1)
+    rz = torch.cat((rz1, rz2, r3), dim = 1)
+
+    rot = rz.matmul(ry).matmul(rx)
+    return rot
+
+
+def random_rot(data_numpy, theta=0.3):
+    """
+    data_numpy: C,T,V,M
+    """
+    data_torch = torch.from_numpy(data_numpy)
+    C, T, V, M = data_torch.shape
+    data_torch = data_torch.permute(1, 0, 2, 3).contiguous().view(T, C, V*M)  # T,3,V*M
+    rot = torch.zeros(3).uniform_(-theta, theta)
+    rot = torch.stack([rot, ] * T, dim=0)
+    rot = _rot(rot)  # T,3,3
+    data_torch = torch.matmul(rot, data_torch)
+    data_torch = data_torch.view(T, C, V, M).permute(1, 0, 2, 3).contiguous()
+
+    return data_torch
